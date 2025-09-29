1b965fe70091b2879e07fb1dddff32430a817c43
lariliar05 <206328467+lariliar05@users.noreply.github.com>
2025-09-29 18:13:38 +0200

    12.C Ikt Java

---
+++ IKT Java/20230913/szamolas.js
@@ -0,0 +1,10 @@
+function teglalap(a, b){
+    //document.getElementById("eredmeny").innerHTML = a*b konnyebb feladatoknal
+    let ter = a*b
+    let eredmeny = document.getElementById("eredmeny")
+    eredmeny.innerHTML ="A területe: " + ter + "cm2"
+
+}
+function kor(a){
+    document.getElementById("eredmeny").innerHTML = Math.sqrt(r) * Math.PI
+}
---
+++ IKT Java/20231129/kcalkalkulator.js
@@ -0,0 +1,25 @@
+//function szamol(){
+//    let kpo = document.querySelector('.kpo').innerHTML;
+//   let perc = document.querySelector('input[type="number"]').value;
+//    let cal = document.querySelector('.cal');
+//    cal.value = (kpo * perc / 60).toFixed();
+//}
+//document.querySelector('input[type="number"]').onchange = szamol 
+
+function szamol(x) {
+    let kpo = document.querySelectorAll('.kpo')[x].innerHTML;
+    let perc = document.querySelectorAll('input[type="number"]')[x].value;
+    let cal = document.querySelectorAll('.cal')[x];
+    cal.value = (kpo * perc / 60).toFixed();
+}
+
+for (let i; i < 10; i++) {
+    document.querySelectorAll('input[type="number"]')[i].onchange = function () {
+        szamol(i)
+
+    }
+}
+
+
+
+
